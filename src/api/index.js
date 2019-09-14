90f9d82d407d9a364a20f95c5560732de1d67f9e
Zhanjiachun <zjc1834552044@163.com>
2019-09-14 17:46:09 +0800

    init gshop

--- src/api/index.js
+++ src/api/index.js
@@ -10,17 +10,17 @@ export const reqFoodTypes = () => ajax('/index_category')
 // 3、根据经纬度获取商铺列表
 export const reqShops = (latitude,longitude) => ajax('/shops',{latitude,longitude})
 // 4、根据经纬度和关键字搜索商铺列表
-export const reqAddress = () => ajax()
+export const reqSerchShops = (geohash,keyword) => ajax('/search_shops',{geohash,keyword})
 // 5、获取一次性验证码
-export const reqAddress = () => ajax()
+export const reqCaptcha = () => ajax('/captcha')
 // 6、用户名密码登陆
-export const reqAddress = () => ajax()
+export const reqNameLogin = (name,pwd,captcha) => ajax('/login_pwd',{name,pwd,captcha},'POST')
 // 7、发送短信验证码
-export const reqAddress = () => ajax()
+export const reqSendCode = (phone) => ajax('/sendcode',{phone})
 // 8、手机号验证码登陆
-export const reqAddress = () => ajax()
+export const reqPhoneLogin = (phone,code) => ajax('login_sms',{phone,code},'POST')
 // 9、根据会话获取用户信息
-export const reqAddress = () => ajax()
+export const reqUserInfo = () => ajax('/userinfo')
 // 10、用户登出
-export const reqAddress = () => ajax()
+export const reqLogOut = () => ajax('/logout')
 
